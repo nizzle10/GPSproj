2ba6a25b0052325f20f643c01dcd850457d3e435
nizcode <neilfa99@gmail.com>
2021-01-27 15:57:26 +0000

    error fixed, the program output is the Highspeed running distance

--- distance_function.R
+++ distance_function.R
@@ -1,11 +1,22 @@
+#!/usr/bin/env Rscript
+
+args <- commandArgs(trailingOnly = TRUE)
+numargs <- length(args)
+enumargs <- 1
+if(numargs != enumargs) {
+   print("Only need 1 gpx file")
+   stop("Stopping right here")
+}
+
 library(geosphere)
-install.packages('DescTools')
+#install.packages('DescTools')
 library(DescTools)
-install.packages('plotKML')
+#install.packages('plotKML')
 library(plotKML)
 library(ggplot2)
 
 
+#GPXd<-readGPX(args)
 GPXd<-readGPX('18_01_21.gpx')
 gpsD<-GPXd$tracks[[1]]$`County Dublin Soccer`
 gpsD$time<-StrExtract(gpsD$time,"\\d\\d:\\d\\d:\\d\\d")
@@ -45,7 +56,7 @@ for (i in c(1:dim(gpsD)[1])){
 }
 
 STD$Speed <- STD$Distances/STD$Seconds
-STD$Times <-gpsD$time[-length(gpsD$time)]
+STD$Times <-gpsD$time#[-length(gpsD$time)]
 
 
 #Last line is always NA
@@ -54,72 +65,88 @@ if(is.na(tail(STD,1))==TRUE){
   
 }
 
-
-
-#Need to approximate all the values, so to have more data
-# use approx function
-plot(STD$Times,STD$Speed,type='l')# plot very similar to garmin connect
-plot(STD$Speed,STD$Distances,type='l')# plot speed vs distance, will be usedul for finding HSRD
-mean(STD$Speed) # mean very similar to garmin connect
-mx <- max(STD$Speed) # max on garmin connect is more
-
-#Problem: How to calculate the distance travelled at 5.5m/s or above?
-#Attempt1: Area under the graph = distance remake, the graph with only speeds above 5.5m/s anything smaller = 0
-#Attempt2: Look at the data and take out all the distances travelled at a certain speed
-#STDx <- approx(STD$Times,STD$Speed)
-#STDx<-data.frame(STDx)
-
-#plot(STDx$x,STDx$y)
-
-
-#Interpolation, makes the data more detailed
-RoxTS<-list()
-#RoxSD<-list()
-#px <-list()
-
-for(i in c(0:(dim(STD)[1]))){
-  a <- STD[(i+1):(i+2),c(3,4)]
-  b <- approx(a$Times,a$Speed)
- # y <- STD[(i+1):(i+2),c(1,3)]
-  #z <- approx(y$Speed,y$Distances)
-  
-  RoxTS$Times[((i*50)+1):((i+1)*50)]<-b$x[1:50]
-  RoxTS$Speed[((i*50)+1):((i+1)*50)]<-b$y[1:50]
-  #RoxSD$Speed[((i*50)+1):((i+1)*50)]<-z$x[1:50]
-  #RoxSD$Distances[((i*50)+1):((i+1)*50)]<-z$y[1:50]
-}
-RoxTS<-data.frame(RoxTS)
-#px<-data.frame(px)
-plot(RoxTS$Times,RoxTS$Speed,type = 'l')
-
-RoxSD<-list()
-
-for(i in c(0:(length(STD$Speed)))){
-  #a <- STD[(i+1):(i+2),c(3,4)]
-  #b <- approx(a$Times,a$Speed)
-  y <- STD[(i+1):(i+2),c(1,3)]
-  print(y)
-  z <- approx(y$Speed,y$Distances)
-  
-  #RoxTS$Times[((i*50)+1):((i+1)*50)]<-b$x[1:50]
-  #RoxTS$Speed[((i*50)+1):((i+1)*50)]<-b$y[1:50]
-  RoxSD$Speed[((i*50)+1):((i+1)*50)]<-z$x[1:50]
-  RoxSD$Distances[((i*50)+1):((i+1)*50)]<-z$y[1:50]
-}
-
-RoxSD<-data.frame(RoxSD)
-plot(RoxSD$Speed,RoxSD$Distances,type = 'p')
-
-
-#plot(px$Times,px$Speed,type = 'l')
-#ggplot(RoxD)
-#Create the min and max speed bracket and filter data to that speed by creating a true and false column
 mn <- 5.5
 mx <- max(STD$Speed)
-RoxSD$bracket <- ifelse(RoxSD$Speed>mn&RoxSD$Speed<mx,TRUE,FALSE)
-RoxTS$bracket <- ifelse(RoxTS$Speed>mn&RoxTS$Speed<mx,TRUE,FALSE)
+#RoxSD$bracket <- ifelse(RoxSD$Speed>mn&RoxSD$Speed<mx,TRUE,FALSE)
+#RoxTS$bracket <- ifelse(RoxTS$Speed>mn&RoxTS$Speed<mx,TRUE,FALSE)
+STD$bracket <- ifelse(STD$Speed>mn&STD$Speed<mx,TRUE,FALSE)
 
+result<-sum(STD$Distances[STD$bracket==TRUE])
+print(result)
 
-plot(RoxTS$Speed[STDx$bracket==TRUE],STDx$y[STDx$bracket==TRUE],type = 'l')
-#Use the Area Under the Curve calculation to find the total distance ran in that speed bracket
-AUC(STDx$x[STDx$bracket==TRUE],STDx$y[STDx$bracket==TRUE],type='step')
+# """
+# #Need to approximate all the values, so to have more data
+# # use approx function
+# #plot(STD$Times,STD$Speed,type='l')# plot very similar to garmin connect
+# #plot(STD$Speed,STD$Distances,type='l')# plot speed vs distance, will be usedul for finding HSRD
+# mean(STD$Speed) # mean very similar to garmin connect
+# mx <- max(STD$Speed) # max on garmin connect is more
+# 
+# #Problem: How to calculate the distance travelled at 5.5m/s or above?
+# #Attempt1: Area under the graph = distance remake, the graph with only speeds above 5.5m/s anything smaller = 0
+# #Attempt2: Look at the data and take out all the distances travelled at a certain speed
+# #STDx <- approx(STD$Times,STD$Speed)
+# #STDx<-data.frame(STDx)
+# 
+# #plot(STDx$x,STDx$y)
+# 
+# 
+# #Interpolation, makes the data more detailed
+# RoxTS<-list()
+# #RoxSD<-list()
+# #px <-list()
+# 
+# for(i in c(0:((dim(STD)[1]-1)))){
+#   a <- STD[(i+1):(i+2),c(3,4)]
+#   b <- approx(a$Times,a$Speed)
+#  # y <- STD[(i+1):(i+2),c(1,3)]
+#   #z <- approx(y$Speed,y$Distances)
+#   
+#   RoxTS$Times[((i*50)+1):((i+1)*50)]<-b$x[1:50]
+#   RoxTS$Speed[((i*50)+1):((i+1)*50)]<-b$y[1:50]
+#   #RoxSD$Speed[((i*50)+1):((i+1)*50)]<-z$x[1:50]
+#   #RoxSD$Distances[((i*50)+1):((i+1)*50)]<-z$y[1:50]
+# }
+# RoxTS<-data.frame(RoxTS)
+# #px<-data.frame(px)
+# #plot(RoxTS$Times,RoxTS$Speed,type = 'l')
+# 
+# RoxSD<-list()
+# 
+# for(i in c(0:((length(STD$Speed)-1)))){
+#   #a <- STD[(i+1):(i+2),c(3,4)]
+#   #b <- approx(a$Times,a$Speed)
+#   y <- STD[(i+1):(i+2),c(1,3)]
+#   #print(y)
+#   z <- approx(y$Speed,y$Distances)
+#   
+#   #RoxTS$Times[((i*50)+1):((i+1)*50)]<-b$x[1:50]
+#   #RoxTS$Speed[((i*50)+1):((i+1)*50)]<-b$y[1:50]
+#   RoxSD$Speed[((i*50)+1):((i+1)*50)]<-z$x[1:50]
+#   RoxSD$Distances[((i*50)+1):((i+1)*50)]<-z$y[1:50]
+# }
+# 
+# RoxSD<-data.frame(RoxSD)
+# #plot(RoxSD$Speed,RoxSD$Distances,type = 'p')
+# 
+# 
+# #plot(px$Times,px$Speed,type = 'l')
+# #ggplot(RoxD)
+# #Create the min and max speed bracket and filter data to that speed by creating a true and false column
+# mn <- 5.5
+# mx <- max(STD$Speed)
+# RoxSD$bracket <- ifelse(RoxSD$Speed>mn&RoxSD$Speed<mx,TRUE,FALSE)
+# RoxTS$bracket <- ifelse(RoxTS$Speed>mn&RoxTS$Speed<mx,TRUE,FALSE)
+# STD$bracket <- ifelse(STD$Speed>mn&STD$Speed<mx,TRUE,FALSE)
+# 
+# #plot(RoxTS$Times[RoxTS$bracket==TRUE],RoxTS$Speed[RoxTS$bracket==TRUE],type = 'l')
+# #plot(RoxSD$Speed[RoxSD$bracket==TRUE],RoxSD$Distances[RoxSD$bracket==TRUE],type = 'p')
+# #Only look at the distances from STD, using approx function does not work when wanting to calculate HSRD
+# result<-sum(STD$Distances[STD$bracket==TRUE])
+# print(result)
+# 
+# 
+# 
+# #Use the Area Under the Curve calculation to find the total distance ran in that speed bracket
+# #AUC(RoxTS$Times[RoxTS$bracket==TRUE],RoxTS$Speed[RoxTS$bracket==TRUE],type='step')
+# """
